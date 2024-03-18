8ceb7eb7c48a0b2f7052d65de90c47fabb804ce9
Ak <akshithgray17@gmail.com>
2024-03-18 18:50:44 +0530

    hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <img src={appleImg} alt="Apple" width={14} height={18} />
             <div className='flex flex-1 justify-center max-sm:hidden'>
                 {navLists.map((nav,i)=>(
-                    <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white translate-all'>
+                    <div key={nav} className='px-5 text-sm cursor-pointer text-gray hover:text-white transition-all'>
                         {nav}
                     </div>
                 ))}
